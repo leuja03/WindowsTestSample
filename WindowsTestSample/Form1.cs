d273aba515a240fd20f6a2754fa372b0ffb6b5ab
Jason Leung <jason.leung@sage.com>
2014-05-05 15:35:31 -0700

    Added bit testing

--- WindowsTestSample/Form1.cs
+++ WindowsTestSample/Form1.cs
@@ -19,15 +19,56 @@ namespace WindowsTestSample
             InitializeComponent();
         }
 
+        public enum BitType
+        {
+           None = 0,
+           One = 1 << 0,
+           Two = 1 << 1,
+           Three = 1 << 2,
+           Four = 1 << 3,
+           Five = 1 << 4,
+           Six = 1 << 5,
+           Seven = 1 << 6,
+           Eight = 1 << 7,
+           Nine = 1 << 8,
+           Ten = 1 << 9,
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
            string result = Test_StaticClass_AltWay.WhatToGet();
 
            TestFactory();
 
+           // Test bits
+           int value = 3;
+           TestAndValue(value);
+           value = 4;
+           TestAndValue(value);
+           value = 10;
+           TestAndValue(value);
+           value = 32;
+           TestAndValue(value);
+           value = 59;
+           TestAndValue(value);
+           value = value << 2;
+
+
             List<string> thisList = new List<string>();
             int count = thisList.Count;
             count = thisList.Capacity;
+        }
+
+        private void TestAndValue(int value)
+        {
+           int testvalue = value & (int)BitType.One;
+           testvalue = value & (int)BitType.Two;
+           testvalue = value & (int)BitType.Three;
+           testvalue = value & (int)BitType.Four;
+           testvalue = value & (int)BitType.Five;
+           testvalue = value & (int)BitType.Six;
+           testvalue = value & (int)BitType.Seven;
+           testvalue = value & (int)BitType.Eight;
         }
 
         private void TestFactory()
