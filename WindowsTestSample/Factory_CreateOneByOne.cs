aeb627fa5a53bf2ee568f5d7266f03093428916f
Jason Leung <jason.leung@sage.com>
2014-05-07 10:22:45 -0700

    Created new file for the test case

---
+++ WindowsTestSample/Factory_CreateOneByOne.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsTestSample
+{
+   /// <summary>
+   /// A class to do testing
+   /// </summary>
+   public class Factory_CreateOneByOne
+   {
+      public static void TestFactory()
+      {
+         var servA = MyServiceFactoryA.CreateStubWithTemperature(45);
+         var servB = MyServiceFactoryB.CreateStubWithContents("test string");
+         var myClass = MyClassFactory.CreateEmpty().WithService(servA).WithOtherService(servB);
+      }
+   }
+
+
+   public interface IServiceA
+   {
+      void DoA();
+   }
+
+   public interface IServiceB
+   {
+      void DoB();
+   }
+
+   public class MyClass
+   {
+      IServiceA m_a;
+      IServiceB m_b;
+
+      public MyClass() { }
+
+      public MyClass(IServiceA a, IServiceB b)
+      {
+         m_a = a;
+         m_b = b;
+      }
+
+      public IServiceA SerA
+      {
+         get { return m_a; }
+         set { m_a = value; }
+      }
+
+      public IServiceB SerB
+      {
+         get { return m_b; }
+         set { m_b = value; }
+      }
+   }
+
+   public static class MyClassExtensions
+   {
+      public static MyClass WithService(this MyClass @this, IServiceA a)
+      {
+         @this.SerA = a;
+         return @this;
+      }
+      public static MyClass WithOtherService(this MyClass @this, IServiceB b)
+      {
+         @this.SerB = b;
+         return @this;
+      }
+   }
+
+   public class MyClassFactory
+   {
+      public static MyClass CreateEmpty()
+      {
+         return new MyClass();
+      }
+      public static MyClass Create(IServiceA a, IServiceB b)
+      {
+         return new MyClass(a, b);
+      }
+   }
+
+   public class ServiceA : IServiceA
+   {
+      public void DoA()
+      {
+         // do a
+      }
+   }
+   public class ServiceB : IServiceB
+   {
+      public void DoB()
+      {
+         // do b
+      }
+   }
+
+   public class MyServiceFactoryA
+   {
+      public static ServiceA CreateStubWithTemperature(int a)
+      {
+         return new ServiceA();
+      }
+   }
+
+   public class MyServiceFactoryB
+   {
+      public static ServiceB CreateStubWithContents(string a)
+      {
+         return new ServiceB();
+      }
+   }
+}
