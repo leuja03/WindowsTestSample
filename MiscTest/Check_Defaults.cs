f77e3a94e36adcee19c4a293c10867cf88f92f59
Jason Leung <jason.leung@sage.com>
2014-10-31 13:11:02 -0700

    Updated

--- MiscTest/Check_Defaults.cs
+++ MiscTest/Check_Defaults.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,24 +11,45 @@ namespace MiscTest
    {
       public static void DoTest()
       {
+         // numeric
          CheckDefault<int> test = new CheckDefault<int>();
          object result = test.Result;
 
+         // string
          CheckDefault<string> stringtest = new CheckDefault<string>();
          result = stringtest.Result;
 
+         // nullable value type
          CheckDefault<int?> nullable = new CheckDefault<int?>();
          result = nullable.Result; // null
 
+         // struct
          CheckDefault<TestStruct> structs = new CheckDefault<TestStruct>();
          result = structs.Result;
          result = new TestStruct(1, 2.2, "3", null, null);
          result = default(TestStruct);
 
+         // reference type
          CheckDefault<TestClass> classs = new CheckDefault<TestClass>();
          result = classs.Result;
 
 
+
+         // Test self-defined default value in reference type
+         CheckDefault<TestDefaultClass> customDefaultClass = new CheckDefault<TestDefaultClass>();
+         result = classs.Result;
+         // constructor
+         TestDefaultClass customValueClass = new TestDefaultClass();
+         bool testValue = customValueClass.MyProperty;
+         testValue = customValueClass.MyProperty2;
+         testValue = customValueClass.Second;
+         testValue = customValueClass.Third;
+
+         // well... the default value attribute is pretty useless!!!!!
+         PropertyDescriptorCollection collection = TypeDescriptor.GetProperties(customValueClass);
+         AttributeCollection attributes = collection["MyProperty2"].Attributes;
+         DefaultValueAttribute myAttribute = attributes[typeof(DefaultValueAttribute)] as DefaultValueAttribute;
+         string defaultValue = myAttribute.Value.ToString();
       }
    }
 
@@ -65,5 +87,39 @@ namespace MiscTest
       public string Second;
       public int? Third;
       public TestStruct Fourth;
+   }
+
+   public class TestDefaultClass
+   {
+      private bool myvalue = false;
+      private bool myvalue2;
+      private bool second;
+
+      [DefaultValue(true)] // this has no effect because the value is defined earlier
+      public bool MyProperty
+      {
+         get { return myvalue; }
+         set { myvalue = value; }
+      }
+
+      [DefaultValue(true)]
+      public bool MyProperty2
+      {
+         get { return myvalue2; }
+         set { myvalue2 = value; }
+      }
+
+      public bool Second
+      {
+         get { return second; }
+         set { second = value; }
+      }
+
+      [DefaultValue(true)]
+      public bool Third
+      {
+         get;
+         set;
+      }
    }
 }
