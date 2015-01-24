804d629f892dda790d83ac5fea91d201b397f76b
Jason Leung <jason.leung@sage.com>
2015-01-23 17:08:38 -0800

    Added unit test to vc++

---
+++ VC_CLR_Lib/VC_CLR_LIB.UnitTest.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace VC_CLR_Lib
+{
+	TEST_CLASS(VC_CLR_LIB)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}
