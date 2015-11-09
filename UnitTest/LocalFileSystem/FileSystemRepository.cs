0500ff13c6fd069a2c8312d379eea0bf9417f992
ccljason <jason.c.leung@gmail.com>
2015-11-09 01:33:30 -0800

    Added Filesystem unit test

---
+++ UnitTest/LocalFileSystem/FileSystemRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace UnitTest.LocalFileSystem
+{
+   public class FileSystemRepository : IFileSystemRepository
+   {
+      private readonly string _Filename;
+
+      public FileSystemRepository(string fileName)
+      {
+         _Filename = fileName;
+      }
+
+      public void Save(EmployeeData something)
+      {
+         using (var sw = new StreamWriter(_Filename, true))
+         {
+            SaveToStream(sw.BaseStream, something);
+         }
+      }
+
+      public EmployeeData Load()
+      {
+         using (var sr = new StreamReader(_Filename))
+         {
+            return LoadFromStream(sr.BaseStream);
+         }
+      }
+
+
+      private void SaveToStream(Stream sw, EmployeeData something)
+      {
+         var serializer = new XmlSerializer(typeof(EmployeeData));
+         serializer.Serialize(sw, something);
+      }
+
+      private EmployeeData LoadFromStream(Stream sr)
+      {
+         var serializer = new XmlSerializer(typeof(EmployeeData));
+         return (EmployeeData)serializer.Deserialize(sr);
+      }
+   }
+}
---
+++ UnitTestProject1/LocalFileSystem/FileSystemUnitTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTest.LocalFileSystem;
+using System.IO;
+
+namespace UnitTestProject1.LocalFileSystem
+{
+   [TestClass]
+   public class FileSystemUnitTest
+   {
+      [TestMethod]
+      [TestCategory("FileSystem")]
+      public void TestMethod1()
+      {
+         var data = new EmployeeData
+         {
+            Id = 1,
+            Name = "First Employee",
+            Email = "First.Employee@email.com",
+         };
+
+         var ms = new MemoryStream();
+         var repository = new FileSystemRepository("sample.xml");
+         PrivateObject privateRepo = new PrivateObject(repository);
+         privateRepo.Invoke("SaveToStream", new Type[] {typeof(Stream), typeof(EmployeeData) }, 
+            new object[] {ms, data});
+
+         string result = ms.ToString();
+
+         ms.Position = 0;
+         EmployeeData loaded = (EmployeeData)privateRepo.Invoke("LoadFromStream", new Type[] { typeof(Stream) },
+            new object[] { ms });
+
+
+         Assert.AreEqual(data.Id, loaded.Id);
+         Assert.AreEqual(data.Name, loaded.Name);
+         Assert.AreEqual(data.Email, loaded.Email);
+      }
+   }
+}
